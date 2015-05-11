696a4ce3e660e77f03c4d0c7f9f453bb3326865c
Peter Lu <rightpeter.lu@gmail.com>
2015-05-11 21:25:41 +0800

    fix server

--- server/operation/dsseCore.py
+++ server/operation/dsseCore.py
@@ -1,23 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import rsa
-import sys
 import os
-import os.path
 import re
 import hashlib
-import random
 import pickle
 import zipfile
+from util import myTool
 
 __author__ = 'Rightpeter'
-
-enc_pattern = re.compile(r'.*\.enc')
-dec_pattern = re.compile(r'.*\.dec')
-pickle_pattern = re.compile(r'.*\.pickle')
-
-keywords_set = []
 
 
 def DSSE_add(username, add_token, enc_file):
@@ -36,33 +27,34 @@ def DSSE_add(username, add_token, enc_file):
 
     filename = add_token[0]
     del add_token[0]
-    filename_sha1 = CalcSha1(filename)
-    filename_md5 = int(CalcMD5(filename), 16)
+    filename_sha1 = myTool.CalcSha1(filename)
+    filename_md5 = int(myTool.CalcMD5(filename), 16)
 
     TD[filename_sha1] = -1 ^ filename_md5
 
     for word in add_token:
-        word_sha1 = CalcSha1(word)
-        word_md5 = int(CalcMD5(word), 16)
+        word_sha1 = myTool.CalcSha1(word)
+        word_md5 = int(myTool.CalcMD5(word), 16)
         try:
             head = TS[word_sha1]
         except:
             head = -1 ^ word_md5
 
-        eTS = TS[CalcSha1("#EMPTY#")] ^ int(CalcMD5("#EMPTY#"), 16)
+        eTS = TS[myTool.CalcSha1("#EMPTY#")] ^ int(myTool.CalcMD5("#EMPTY#"), 16)
         eAsNext = AS[eTS][1]
         AS[eTS] = [int(filename) ^ word_md5, head]
 
-        eTD = TD[CalcSha1("#EMPTY#")] ^ int(CalcMD5("#EMPTY#"), 16)
+        eTD = TD[myTool.CalcSha1("#EMPTY#")] ^ int(myTool.CalcMD5("#EMPTY#"), 16)
         eAdNext = AD[eTD][1]
-        AD[eTD] = [sxor(word, CalcMD5(filename)), TD[filename_sha1]]
-        # AD[TD[CalcSha1("#EMPTY#")]] = [TS[CalcSha1("#EMPTY#")], TD[CalcSha1(filename)]]
+        AD[eTD] = [myTool.sxor(word, myTool.CalcMD5(filename)), TD[filename_sha1]]
+        # AD[TD[myTool.CalcSha1("#EMPTY#")]] =
+        # [TS[myTool.CalcSha1("#EMPTY#")], TD[myTool.CalcSha1(filename)]]
 
         TS[word_sha1] = eTS ^ word_md5
-        TS[CalcSha1("#EMPTY#")] = eAsNext
+        TS[myTool.CalcSha1("#EMPTY#")] = eAsNext
 
         TD[filename_sha1] = eTD ^ filename_md5
-        TD[CalcSha1("#EMPTY#")] = eAdNext
+        TD[myTool.CalcSha1("#EMPTY#")] = eAdNext
 
     with open(os.path.join(rootdir, 'TS.pickle'), 'w+') as outputfile:
         pickle.dump(TS, outputfile)
@@ -95,26 +87,26 @@ def DSSE_del(username, filename):
     with open(os.path.join(rootdir, 'AD.pickle'), 'r') as picklefile:
         AD = pickle.load(picklefile)
 
-    filename_sha1 = CalcSha1(filename)
-    filename_md5 = CalcMD5(filename)
+    filename_sha1 = myTool.CalcSha1(filename)
+    filename_md5 = myTool.CalcMD5(filename)
     filename_md5_hex = int(filename_md5, 16)
 
-    print 'TD[filename_sha1]: ', TD[filename_sha1] ^ Filename_md5_hex
+    print 'TD[filename_sha1]: ', TD[filename_sha1] ^ filename_md5_hex
     fHere = TD[filename_sha1] ^ filename_md5_hex
-    # del TD[CalcSha1(filename)]
+    # del TD[myTool.CalcSha1(filename)]
 
     print '-----------------DSSE_del------------------\n'
     while fHere != -1:
         fNext = AD[fHere][1] ^ filename_md5_hex
-        word = sxor(AD[fHere][0], filename_md5)
-        word_sha1 = CalcSha1(word)
-        word_md5 = int(CalcMD5(word), 16)
+        word = myTool.sxor(AD[fHere][0], filename_md5)
+        word_sha1 = myTool.CalcSha1(word)
+        word_md5 = int(myTool.CalcMD5(word), 16)
         print 'AD[fHere][0]: ', AD[fHere][0]
         print 'fNext: ', fNext
         print 'word: ', word
 
         print AS[TS[word_sha1] ^ word_md5][0] ^ word_md5
-        if CalcSha1(str(AS[TS[word_sha1] ^ word_md5][0] ^ word_md5)) == filename_sha1:
+        if myTool.CalcSha1(str(AS[TS[word_sha1] ^ word_md5][0] ^ word_md5)) == filename_sha1:
             j = TS[word_sha1] ^ word_md5
             TS[word_sha1] = AS[j][1]
         else:
@@ -127,11 +119,11 @@ def DSSE_del(username, filename):
             if j != -1:
                 AS[i][1] = AS[j][1]
 
-        AS[j] = ["#EMPTY#", TS[CalcSha1("#EMPTY#")]]
-        TS[CalcSha1("#EMPTY#")] = j ^ int(CalcMD5("#EMPTY#"), 16)
+        AS[j] = ["#EMPTY#", TS[myTool.CalcSha1("#EMPTY#")]]
+        TS[myTool.CalcSha1("#EMPTY#")] = j ^ int(myTool.CalcMD5("#EMPTY#"), 16)
 
-        AD[fHere] = ["#EMPTY#", TD[CalcSha1("#EMPTY#")]]
-        TD[CalcSha1("#EMPTY#")] = fHere ^ int(CalcMD5("#EMPTY#"), 16)
+        AD[fHere] = ["#EMPTY#", TD[myTool.CalcSha1("#EMPTY#")]]
+        TD[myTool.CalcSha1("#EMPTY#")] = fHere ^ int(myTool.CalcMD5("#EMPTY#"), 16)
         fHere = fNext
 
     with open(os.path.join(rootdir, 'TS.pickle'), 'w+') as outputfile:
